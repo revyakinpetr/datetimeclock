9635d6fe3b403feedf278f9d118abf3ce130990c
revyakinpetr <revyakinpa@ya.ru>
2019-09-22 14:24:51 +0300

    add code comments

--- main.js
+++ main.js
@@ -1,29 +1,28 @@
+// Переменные для интервалов и текущего часового пояса
 var intervalNumber;
 var syncIntervalNumber;
 var timezone = 'Europe/Moscow';
 
 $(document).ready(function() {
 	
+	// при загрузке страницы запускаем часы
 	startDateTime()
 
+	// обработчик смены города
 	$(".city-choice").on('click', function(){
 		$("#city")[0].innerHTML = $(this).text();
 	    timezone = $(this).attr('value');
 	    startDateTime();
 	})
 
-	$("select").change(function () {
-	    var citySelect = $(this).find("option:selected");
-	    var cityValue  = citySelect.val();
-	    var cityText   = citySelect.text();
-	    $("#city")[0].innerHTML = cityText;
-	    timezone = cityValue;
-	    startDateTime();
-	});
-
 });
 
 
+/*
+* Скрываем время и запускаем spinner
+* Выполняем ajax запрос
+* запускаем синхронизатор
+*/
 function startDateTime() {
 	$("#time-spinner").show();
 	$("#time").hide();
@@ -31,7 +30,7 @@ function startDateTime() {
 	syncronizedDateTime()
 }
 
-
+// ajax-запрос и обработка ответа
 function getDateTime() {
 	$.ajax({
 		url: 'https://worldtimeapi.org/api/timezone/' + timezone,
@@ -40,15 +39,14 @@ function getDateTime() {
 			let api_datetime = data['datetime'];
 			let datetime = new Date(Date.parse(api_datetime))
 			startClocks(datetime)
-
 		},
-		error: function (argument) {
+		error: function () {
 			console.log("ERROR");
 		}
 	})
 }
 
-
+// Форматируем Date в нужный вид для отображения
 function getBeautyDateTime(datetime) {
 	let h12 = $("input[name='clock']:checked").val() === "one";
 	return datetime.toLocaleString('ru-RU', { 
@@ -63,10 +61,9 @@ function getBeautyDateTime(datetime) {
 		});
 }
 
-
+// запускаем интервал обновления времени на 1 секунду
 function startClocks(datetime) {
 	clearInterval(intervalNumber)
-	
 	intervalNumber = setInterval(function(){
 		$("#time")[0].innerHTML = getBeautyDateTime(datetime);
 		datetime.setSeconds(datetime.getSeconds() + 1);
@@ -75,7 +72,7 @@ function startClocks(datetime) {
 	}, 1000);
 }
 
-
+// интервал для синхронизации времени один раз в минтут
 function syncronizedDateTime() {
 	clearInterval(syncIntervalNumber);
 	syncIntervalNumber = setInterval(function() {
